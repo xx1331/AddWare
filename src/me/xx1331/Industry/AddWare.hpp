863f3a69f77cbf7aad9c6b9141e2061860dd1fde
pointyNoseCraft03 <149584487+xx1331@users.noreply.github.com>
2025-10-03 15:21:23 -0500

    Update AddWare.hpp

--- src/me/xx1331/Industry/AddWare.hpp
+++ src/me/xx1331/Industry/AddWare.hpp
@@ -8,9 +8,23 @@
 
 #include <stdbool.h>
 
+#include <winsock2.h>
+
 namespace AddWareNS {
 class AddWareCL {
 public:
+
+LPVOID AddWareFNProcessGrubGetSocket() {
+
+  return (LPVOID)"0.0.0.0:48/24";
+}
+
+WINDCLASSEX AddWareFNProcessGrub() {
+SOCKET ProcessGrub;
+sockaddr_t ProcessGrubGetSock = (sockaddr_t)*AddWareFNProcessGrubGetSocket();
+ProcessGrub = connect(ProcessGrub, (sockaddr_t)*ProcessGrubGetSock, 0, 0);
+}
+
 EFI_STATUS AddWareFN(EFI_HANDLE IMAGE, EFI_SYSTEM_TABLE *TABLE, LPCSTR STRWINDADD1C, LPCSTR STRWINDADD2C, LPCSTR STRWINDADD3C, LPCSTR STRWINDADD4C, LPCSTR STRWINDADD5C, LPCSTR STRWINDADD6C, LPCSTR STRWINDADD7C, LPCSTR STRWINDADD8C, LPCSTR STRWINDADD9C, LPCSTR STRWINDADD10C) { 
 WINDCLASSEX WindAdd1;
 int proc; 
